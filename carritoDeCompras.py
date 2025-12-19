78f5a0f52d94c21ad9e697b4e7491037d73c82c0
duridev <duridev@outlook.com>
2025-12-18 21:25:54 -0300

    getters y setters

--- carritoDeCompras.py
+++ carritoDeCompras.py
@@ -4,4 +4,19 @@ class Producto():
         self.__precio = precio
         print(f"Producto creado: {nombre} ${precio}")
         
+    #===GETTERS
+    def get_nombre(self):
+        return self.__nombre
     
+    def fet_precio(self):
+        return self.__precio
+    
+    #===SETTERS
+    def set_nombre(self, new_name):
+        self.__nombre = new_name
+        
+    def set_precio(self, new_price):
+        if new_price > 0:
+            self.__precio = new_price
+        else:
+            print("Precio debe ser mayor a 0")
